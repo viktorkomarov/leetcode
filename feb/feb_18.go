d95559303fd786128a87ab0cae4bc3b8a91525ae
Viktor Komarov <victorkomarov98@mail.ru>
2021-02-18 18:56:34 +0300

    dynamic approach

--- feb/feb_18.go
+++ feb/feb_18.go
@@ -1,35 +1,15 @@
 package feb
 
-func isArithmetic(num []int) (int, bool) {
-	step := num[1] - num[0]
-	if num[2]-num[1] != step {
-		return 0, false
-	}
-
-	return step, true
-}
-
 func numberOfArithmeticSlices(A []int) int {
-	if len(A) < 3 {
-		return 0
+	dp := make([]int, len(A))
+	sum := 0
+
+	for i := 2; i < len(A); i++ {
+		if A[i]-A[i-1] == A[i-1]-A[i-2] {
+			dp[i] += 1 + dp[i-1]
+		}
+		sum += dp[i]
 	}
 
-	count := 0
-	for i := 0; i < len(A)-2; i++ {
-		step, ok := isArithmetic(A[i : i+3])
-		if !ok {
-			continue
-		}
-
-		count++
-		for j := i + 3; j < len(A); j++ {
-			if A[j]-A[j-1] == step {
-				count++
-			} else {
-				break
-			}
-		}
-	}
-
-	return count
+	return sum
 }
